1568026f4af5f930ec14088b6d468de37457e237
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2026-02-04 20:56:48 +0100

    Add diffs.nvim to make vim-fugitive highlights better

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -10,7 +10,7 @@ return {
     'sindrets/diffview.nvim',
     opts = {},
     keys = {
-      { '<leader>gvo', '<cmd>DiffviewOpen<cr>', desc = '[diffview]: Open' },
+      { '<leader>gvo', '<cmd>DiffviewOpen<cr>',  desc = '[diffview]: Open' },
       { '<leader>gvc', '<cmd>DiffviewClose<cr>', desc = '[diffview]: Close' },
     },
   },
@@ -18,16 +18,19 @@ return {
   -- A Git wrapper so awesome, it should be illegal
   {
     'tpope/vim-fugitive',
-    dependencies = { 'tpope/vim-rhubarb' },
+    dependencies = {
+      'tpope/vim-rhubarb',
+      'barrettruth/diffs.nvim'
+    },
     commands = { 'Git' },
     keys = {
       { '<leader>g<Space>', ':Git<Space>' },
-      { '<leader>gs', '<CMD>Git<cr>' },
-      { '<leader>gw', '<CMD>Gwrite<cr>' },
-      { '<leader>ga', '<CMD>Gadd<cr>' },
-      { '<leader>gb', '<CMD>Git blame<CR>' },
-      { '<leader>gl', '<CMD>Git bl<CR>' },
-      { '<leader>ghp', ':!gh pr view -w || gh pr create -w<cr>', { desc = 'Create or view GitHub PR' } },
+      { '<leader>gs',       '<CMD>Git<cr>' },
+      { '<leader>gw',       '<CMD>Gwrite<cr>' },
+      { '<leader>ga',       '<CMD>Gadd<cr>' },
+      { '<leader>gb',       '<CMD>Git blame<CR>' },
+      { '<leader>gl',       '<CMD>Git bl<CR>' },
+      { '<leader>ghp',      ':!gh pr view -w || gh pr create -w<cr>', { desc = 'Create or view GitHub PR' } },
     },
   },
 }
