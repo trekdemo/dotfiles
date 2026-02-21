474637310aa1886fc4ee197438792809e4ed0aa7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2026-02-21 18:27:02 +0100

    Aliases for claude and opencode

--- home/.config/zsh/conf.d/aliases.zsh
+++ home/.config/zsh/conf.d/aliases.zsh
@@ -2,3 +2,6 @@ list="eza --icons=auto --hyperlink --group-directories-first --git"
 alias l="${list}"
 alias ll="${list} --long --no-time --no-user --no-permissions --octal-permissions"
 alias la="${list} --long --no-time --no-user --no-permissions --octal-permissions --all"
+
+alias oc=opencode
+alias cl=claude
