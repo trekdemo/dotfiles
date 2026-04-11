ea361dde34ce76cefa787d774f281502da5c8e4a
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2026-04-11 19:52:00 +0200

    Add obsidian access to neovim

---
+++ home/.config/nvim/lua/plugins/obsidian-nvim.lua
@@ -0,0 +1,21 @@
+return {
+  'obsidian-nvim/obsidian.nvim',
+  -- version = "*", -- use latest release, remove to use latest commit
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in the next major release
+    templates = {
+      folder = 'templates',
+    },
+    daily_notes = {
+      folder = 'Journal',
+    },
+    workspaces = {
+      {
+        name = 'Vault',
+        path = '~/Downloads/Notes/',
+      },
+    },
+  },
+}
