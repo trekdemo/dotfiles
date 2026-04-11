f628adfdc6abee02bd6e492cb560929acd825246
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2026-04-11 19:49:18 +0200

    Try to use Diffview more
    I find myself going back and forth between lazygit and neovim to edit
    some files. It would be great to stay within vim.

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -9,9 +9,9 @@ return {
   {
     'sindrets/diffview.nvim',
     opts = {},
+    commands = { 'DiffviewOpen' },
     keys = {
-      { '<leader>gvo', '<cmd>DiffviewOpen<cr>',  desc = '[diffview]: Open' },
-      { '<leader>gvc', '<cmd>DiffviewClose<cr>', desc = '[diffview]: Close' },
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[diffview]: Open' },
     },
   },
 
@@ -27,17 +27,17 @@ return {
           neogit = false,
           extra_filetypes = { 'diff' },
         }
-      end
+      end,
     },
     commands = { 'Git' },
     keys = {
       { '<leader>g<Space>', ':Git<Space>' },
-      { '<leader>gs',       '<CMD>Git<cr>' },
-      { '<leader>gw',       '<CMD>Gwrite<cr>' },
-      { '<leader>ga',       '<CMD>Gadd<cr>' },
-      { '<leader>gb',       '<CMD>Git blame<CR>' },
-      { '<leader>gl',       '<CMD>Git bl<CR>' },
-      { '<leader>ghp',      ':!gh pr view -w || gh pr create -w<cr>', { desc = 'Create or view GitHub PR' } },
+      { '<leader>gs', '<CMD>Git<cr>' },
+      { '<leader>gw', '<CMD>Gwrite<cr>' },
+      { '<leader>ga', '<CMD>Gadd<cr>' },
+      { '<leader>gb', '<CMD>Git blame<CR>' },
+      { '<leader>gl', '<CMD>Git bl<CR>' },
+      { '<leader>ghp', ':!gh pr view -w || gh pr create -w<cr>', { desc = 'Create or view GitHub PR' } },
     },
   },
 }
