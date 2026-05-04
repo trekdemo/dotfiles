7eb50a2d0986d0c5f650c4f344af126ecddab9a0
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2026-05-04 10:43:08 +0200

    Enable neovim-remote from coding harnesses

--- home/.config/zsh/conf.d/neovim.zsh
+++ home/.config/zsh/conf.d/neovim.zsh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+export EDITOR="nvim"
+
+export NVIM_LISTEN_ADDRESS="/tmp/nvimsocket"
 if [ -z "$NVIM_LISTEN_ADDRESS" ]; then
-  export VISUAL=nvim
+  # Kept for cases where listen address is unset
+  export VISUAL="$EDITOR"
 else
   export VISUAL="nvr -cc split --remote-wait +'setlocal bufhidden=wipe'"
 fi
-export EDITOR="$VISUAL"
