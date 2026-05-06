2c384eea0f7dda91efefcb642efc6f09fb89bed0
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2026-05-06 11:27:52 +0200

    Remove nvim listen address
    This prevented starting more than one nvim

--- home/.config/zsh/conf.d/neovim.zsh
+++ home/.config/zsh/conf.d/neovim.zsh
@@ -2,9 +2,7 @@
 
 export EDITOR="nvim"
 
-export NVIM_LISTEN_ADDRESS="/tmp/nvimsocket"
 if [ -z "$NVIM_LISTEN_ADDRESS" ]; then
-  # Kept for cases where listen address is unset
   export VISUAL="$EDITOR"
 else
   export VISUAL="nvr -cc split --remote-wait +'setlocal bufhidden=wipe'"
