1420103ccd81ea64d45326ea69fb271e711ccec6
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2026-09-02 20:47:59 +0200

    Add quick edit alias eff

--- home/.config/zsh/conf.d/fzf.zsh
+++ home/.config/zsh/conf.d/fzf.zsh
@@ -3,6 +3,13 @@
 # https://github.com/junegunn/fzf?tab=readme-ov-file#fuzzy-completion-for-bash-and-zsh
 source <(fzf --zsh)
 
+if [[ "$TERM" == "xterm-kitty" ]]; then
+  alias ff="fzf --preview 'case \$(file --mime-type -b {}) in image/*) kitty icat --clear --transfer-mode=memory --stdin=no --place=\${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 {} ;; *) bat --style=numbers --color=always {} ;; esac'"
+else
+  alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
+fi
+alias eff='$EDITOR "$(ff)"'
+
 # Add FZF completion for brew commands
 # Type brew <command> **<Tab>
 _fzf_complete_brew() {
